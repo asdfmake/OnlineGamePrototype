e59a735df691e2ab328f6ecaad6c2fcacf699658
asdfmake <makevic2003@gmail.com>
2022-04-16 01:14:03 +0200

    Added win line
    Added win line, logic for endgame and animations for win line
    Missing animation for diagonal line tho

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ function UpdateGrid(NewGrid){
 
 }
 
-function Pick(e){
-    //check if field is empty
+function Pick(e){//gotta check if there is more playable space
     let coordinates = {
         row: e.target.getAttribute("data-row"),
         col: e.target.getAttribute("data-column")
@@ -56,6 +55,7 @@ function CheckGrid(){
         if(grid[i][0] == grid[i][1] && grid[i][0] == grid[i][2]){
             if(grid[i][0] != null){
                 console.log("column");
+                Win("horizontal", i, grid[i][0]);
             }
             
         }
@@ -66,20 +66,83 @@ function CheckGrid(){
         if(grid[0][i] == grid[1][i] && grid[0][i] == grid[2][i]){
             if(grid[0][i] != null){
                 console.log("row");
+                Win("vertical", i, grid[0][i]);
             }
             
         }
         
     }
     //diagonals
-    if((grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) || (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])){
+    if(grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]){
         if(grid[1][1] != null){
-            console.log("diagonal")
+            console.log("diagonal1")
+            Win("diagonal", 4, grid[1][1])
         }
-        
+    }
+
+    if(grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0]){
+        if(grid[1][1] != null){
+            console.log("diagonal2")
+            Win("diagonal", 5, grid[1][1])
+        }
     }
 
 }
 
-Array.from(document.getElementsByClassName("field")).forEach(element => element.addEventListener("click", Pick));
 
+function Win(axis, index, winner){
+    gridHolder.style.pointerEvents = "none";
+    let winLine = document.getElementById("winLine");
+    let Multiplier;
+
+    switch(index){
+        case 0:
+            Multiplier = 1;
+            break;
+        case 1:
+            Multiplier = 3;
+            break;
+        case 2:
+            Multiplier = 5;
+            break;
+        //case for diagonals fi 4 multiplier is -1 if 5 mulitplier 1
+        case 4:
+            Multiplier = 1;
+            break;
+        case 5:
+            Multiplier = -1;
+            break;
+    }
+
+    
+    switch(axis){
+        case "horizontal":
+            winLine.classList.add("horizontal-win");
+            winLine.style.top = "calc(16.6% * " + Multiplier + " - 5px)";
+            break;
+        case "vertical":
+            winLine.classList.add("vertical-win");
+            winLine.style.left = "calc(16.6% * " + Multiplier + " - 2px)";
+            break;
+        case "diagonal":
+            winLine.classList.add("diagonal-win");
+            winLine.style.transform = "rotate(" + 45 * Multiplier + "deg)"
+            break;
+    }
+
+    //winLine.classList.add("horizontal-win")
+
+    console.log("winner is: " + winner.charAt(13) )
+
+    setTimeout(() => {
+        winLine.classList.remove("horizontal-win")
+        winLine.classList.remove("vertical-win")
+        winLine.classList.remove("diagonal-win")
+        winLine.style = null
+        //trigger endscreen or sum bullshit
+        //reset game
+    }, 2000);
+}
+
+
+Array.from(document.getElementsByClassName("field")).forEach(element => element.addEventListener("click", Pick));
